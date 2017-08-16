cc1f84229818e944fee9140f473e70b721e5b98b
Carlos Schults <carlos.schults@gmail.com>
2017-08-16 13:12:55 -0300

    Criação do primeiro método de teste.

--- PrimeiraClasseDeTeste.cs
+++ PrimeiraClasseDeTeste.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace PrimeiroTeste
 {
     public class PrimeiraClasseDeTeste
     {
+        [Test]
+        public void PrimeiroMetodoDeTeste()
+        {
+
+        }
     }
 }
