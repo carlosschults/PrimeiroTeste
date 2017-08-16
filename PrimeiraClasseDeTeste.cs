5fa558dc75674bf48978f1a10a88938e7a8201e5
Carlos Schults <carlos.schults@gmail.com>
2017-08-16 13:34:16 -0300

    Adicionar várias asserções comuns.

--- PrimeiraClasseDeTeste.cs
+++ PrimeiraClasseDeTeste.cs
@@ -12,7 +12,25 @@ namespace PrimeiroTeste
         [Test]
         public void PrimeiroMetodoDeTeste()
         {
-            Assert.AreEqual(10, 5 + 5);
-        }
+            var listaVazia = new List<int>();
+            string stringVazia = "";
+            string stringComEspacoEmBranco = " ";
+            var strings = new List<string>();
+            strings.Add(null);
+            strings.Add("uma string válida");
+            strings.Add("outra string válida");
+
+            Assert.AreEqual("uma string válida", strings[1]);
+            Assert.AreNotEqual("uma string válida!!!", strings[1]);
+            Assert.Null(strings[0]);
+            Assert.NotNull(strings[2]);
+            Assert.Contains("outra string válida", strings);
+            Assert.True(strings.Count == 3);
+            Assert.False(strings.Count != 3);            
+            Assert.IsEmpty(stringVazia);
+            Assert.IsNotEmpty(stringComEspacoEmBranco);
+            Assert.IsEmpty(listaVazia);
+            Assert.That(() => int.Parse("dez"), Throws.TypeOf<FormatException>());
+        }        
     }
 }
