af586666e6188be7c6a227645676becb08091408
Carlos Schults <carlos.schults@gmail.com>
2017-08-16 13:36:41 -0300

    Adicionei classe FizzBuzz.

---
+++ FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroTeste
+{
+    class FizzBuzz
+    {
+        public static string Calcular(int n)
+        {
+            var resposta = string.Empty;
+            if (n % 3 == 0)
+                resposta += "Fizz";
+            if (n % 5 == 0)
+                resposta += "Buzz";
+
+            return resposta == string.Empty ? n.ToString() : resposta;
+        }
+    }
+}
